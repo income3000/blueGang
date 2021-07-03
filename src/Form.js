8d1f76416faf76d32899c57d0b0f8e99865c197c
income3000 <income3000@gmail.com>
2021-07-03 16:48:42 -0400

    done

--- src/Form.js
+++ src/Form.js
@@ -9,9 +9,9 @@ export default function Form({searchString, setSearchString}) {
     return (
         <div className="textBar">
             
-           
+           <form id="thisForm">
          <input type="text" className="cityBox" placeholder="ENTER STATE" onChange={((event)=> setSearchString(event.target.value))}/>
-            
+         </form>
             
         </div>
     )
